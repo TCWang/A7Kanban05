dedf0b024746bbcde1db82e62c017f502c2119d9
TC Wang <wangtct@hotmail.com>
2023-09-11 11:13:13 +0800

    bootstrap

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,9 +37,10 @@ export default {
     ],
     script: [
       {
-        src: '/js/bootstrap.bundle.min.js',
-        type: 'text/javascript',
-        mode: 'client',
+        src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js',
+        integrity:
+          'sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm',
+        crossorigin: 'anonymous',
       },
       // {
       //   src: '/js/bootstrap.bundle.min.js',
